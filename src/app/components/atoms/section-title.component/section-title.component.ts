86e1255b1a16de8779d2b8317f7f71fb4fbc621e
VRLuis <luisvillarroldan1010@gmail.com>
2026-08-13 08:53:03 +0200

    feat: creating a section title for normalizing section titles

---
+++ src/app/components/atoms/section-title.component/section-title.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-title',
+  standalone: true,
+  templateUrl: './section-title.component.html',
+  styleUrl: './section-title.component.css',
+})
+export class SectionTitleComponent {
+  @Input() title = '';
+  @Input() subTitle = '';
+}
