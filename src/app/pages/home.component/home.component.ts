4d3b75d62dc2dcbbf13c4d393d207bfbce2800dc
VRLuis <luisvillarroldan1010@gmail.com>
2026-08-14 12:59:52 +0200

    Adding title in the section of the management

--- src/app/pages/home.component/home.component.ts
+++ src/app/pages/home.component/home.component.ts
@@ -4,10 +4,11 @@ import { CreateRestaurantFormComponent } from "../../components/molecules/create
 import { RestaurantInterface } from '../../interfaces/restaurant/restaurant-interface';
 import { RestaurantGlobalService } from '../../services/global/restaurant-global.service';
 import { ServiceTableListComponent } from '../../components/organisms/service-table-list.component/service-table-list.component';
+import { SectionTitleComponent } from "../../components/atoms/section-title.component/section-title.component";
 
 @Component({
   selector: 'app-home.component',
-  imports: [CreateRestaurantFormComponent, ServiceTableListComponent],
+  imports: [CreateRestaurantFormComponent, ServiceTableListComponent, SectionTitleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
